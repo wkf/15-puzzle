8706caa95118cc6db5ec1b9214ef66cfbd42ddf0
Will Farrell <will@mojotech.com>
2012-08-24 09:57:53 -0400

    removed gosu code

--- application.rb
+++ application.rb
@@ -1,15 +1,3 @@
-require 'gosu'
-
-class MyWindow < Gosu::Window
-  def initialize
-    super(640, 480, false)
-    self.caption = 'Hello World!'
-  end
-end
-
-#window = MyWindow.new
-#window.show
-
 
 class Puzzle
 
